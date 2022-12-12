a404e8dffff6192b0f3b6336694ec532b9bf914b
WeiKai-Hsu <N26104890@gs.ncku.edu.tw>
2022-12-12 18:15:45 +0800

    Commit Message 1. add CSRLU.sv for CSR inst. operation

---
+++ CPU/CSRLU.sv
@@ -0,0 +1,23 @@
+`include "../include/Config.svh" 
+
+module CSRLU(
+    input [31:0] csr,
+    input [31:0] rs1,
+    input [2:0] funct3,
+
+    output logic [31:0] CSRLUOut
+);
+
+always_comb begin
+    case(funct3)
+        `RW  : CSRLUOut = rs1;
+        `RWI : CSRLUOut = rs1;
+        `RS  : CSRLUOut = csr | rs1;
+        `RSI : CSRLUOut = csr | rs1;
+        `RC  : CSRLUOut = csr & (~rs1);
+        `RCI : CSRLUOut = csr & (~rs1);
+        default: CSRLUOut = 32'b0;
+    endcase
+end
+
+endmodule
